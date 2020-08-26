47f7e8de1f6591b990012d4631308ce9a1021f08
DIPENDRA KUMAR SHAKYA <58310946+dkshakya04@users.noreply.github.com>
2020-08-27 00:46:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Miet | ERP </title>
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css" />
 	
 </head>
 
